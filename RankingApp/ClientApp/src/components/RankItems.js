c349f14841461aa8eacf67b1e2ae622ed75c8156
Cemre YARDIM <cemreyardim@gmail.com>
2023-09-08 00:33:02 +0300

    Ranking and Jokes Apps

---
+++ RankingApp/ClientApp/src/components/RankItems.js
@@ -0,0 +1,83 @@
+import {useEffect, useState} from 'react';
+import MovieImageArr from './MovieImages';
+import RankingGrid from './RankingGrid';
+import ItemCollection from './ItemCollection';
+
+const RankItems = ({items, setItems, dataType, imgArr, localStorageKey}) => {
+
+    const [reload, setReload] = useState(false);
+
+    function Reload() {
+        setReload(true);
+    }
+    
+    function drag(ev) {
+        ev.dataTransfer.setData("text", ev.target.id);
+    }
+
+    function allowDrop(ev) {
+        ev.preventDefault();
+    }
+
+    function drop(ev) {
+        ev.preventDefault();
+        const targetElement = ev.target;
+        if(targetElement.nodeName === "IMG") {
+            return false;
+        }
+        if(targetElement.childNodes.length === 0) {
+            var data = parseInt(ev.dataTransfer.getData("text").substring(5));
+            const transformedCollection = items.map((item) => (item.id === parseInt(data)) ?
+             {...item, ranking: parseInt(targetElement.id.substring(5)) } : {...item, ranking: item.ranking });
+
+            setItems(transformedCollection); 
+        }
+    }
+
+    useEffect(() => {
+        if(items == null) {
+            getDataFromApi();
+        }
+        
+    },[dataType]);
+
+    function getDataFromApi() {
+        fetch(`item/${dataType}`)
+        .then((results) => {
+            return results.json();
+        })
+        .then(data => {
+            setItems(data);
+        })
+    }
+
+    useEffect(() => {
+        if(items != null) {
+            localStorage.setItem(localStorageKey, JSON.stringify(items));
+        }
+        setReload(false);
+        
+
+    },[items])
+
+    useEffect(() => {
+        if(reload === true) {
+            getDataFromApi();
+        }
+
+    },[reload])
+
+
+    return (
+        (items != null)?
+        <main>
+            <RankingGrid items={items} imgArr={imgArr} drag={drag} allowDrop={allowDrop} drop={drop} />
+
+            <ItemCollection items ={items} drag={drag} imgArr={imgArr} />
+
+            <button onClick={Reload} className="reload" style={{"marginTop": "10px"}}>Reload</button>
+        </main>
+        : <main>Loading...</main>
+    )
+}
+export default RankItems;
